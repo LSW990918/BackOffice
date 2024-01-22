a558b4193ab7d6e30ba25f61ac29c4e046946b04
LSW990918 <leesywon99@gmail.com>
2024-01-22 15:24:51 +0900

    test

---
+++ src/test/kotlin/com/b2/backoffice/BackOfficeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.b2.backoffice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackOfficeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
