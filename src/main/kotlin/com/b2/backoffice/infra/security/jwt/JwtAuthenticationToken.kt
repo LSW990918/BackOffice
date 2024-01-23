78391230d62131ed46b8469b13bd2dcc72a8c900
codingjang <jjangga813@naver.com>
2024-01-23 17:47:14 +0900

    JwtAuthenticationToken 작성
    사용자 정보와 권한을 포함하는 토큰 생성.
    사용자가 인증되었음을 나타냄.

---
+++ src/main/kotlin/com/b2/backoffice/infra/security/jwt/JwtAuthenticationToken.kt
@@ -0,0 +1,24 @@
+package com.b2.backoffice.infra.security.jwt
+
+import com.b2.backoffice.infra.security.UserPrincipal
+import org.hibernate.metamodel.mapping.TableDetails.KeyDetails
+import org.springframework.security.authentication.AbstractAuthenticationToken
+import org.springframework.security.web.authentication.WebAuthenticationDetails
+
+class JwtAuthenticationToken(
+    private val principal: UserPrincipal,
+    details: WebAuthenticationDetails
+): AbstractAuthenticationToken(principal.authoricies)
+{
+    init{
+        super.setAuthenticated(true)
+        super.setDetails(details)
+    }
+    override fun getPrincipal() = null
+    override fun getCredentials() = null
+
+    override fun isAuthenticated(): Boolean {
+        return true
+    }
+
+}
