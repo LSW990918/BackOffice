294540d7e7117c58f4c0a1fd9edd50e21e381b80
codingjang <jjangga813@naver.com>
2024-01-23 15:51:14 +0900

    Spring Security 의존성 작성 - gradle - main 에 자동설정 방지 ( exclude ) 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -51,6 +51,13 @@ dependencies {
     // 어플리케이션이 실행될 때만 DB 드라이버를 설치하겠다.
     runtimeOnly("org.postgresql:postgresql")
 
+    // Spring Security 추가
+    implementation("org.springframework.boot:spring-boot-starter-security")
+
+    // jwt 관련 라이브러리 중 jjwt 추가
+    implementation("io.jsonwebtoken:jjwt-api:0.12.3")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.3")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.3")
 
     // H2 database
     implementation("com.h2database:h2")
