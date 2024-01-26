0a5950c6d48756fe2698c8f53add1b9cb25cc9a6
cresent10 <spo_pok@naver.com>
2024-01-26 16:48:19 +0900

    Post Controller, Post dto 등 수정
    post 테스트 완료

--- src/main/kotlin/com/b2/backoffice/domain/post/dto/PostUpdateRequest.kt
+++ src/main/kotlin/com/b2/backoffice/domain/post/dto/PostUpdateRequest.kt
@@ -1,6 +1,7 @@
 package com.b2.backoffice.domain.post.dto
 
 import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Pattern
 
 data class PostUpdateRequest(
     @field:NotNull(message = "Title must not be null")
